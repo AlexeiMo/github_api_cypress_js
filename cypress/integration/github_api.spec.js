2b28ab4a04563cadce1683cdd05eac4aa8fcc4ea
RuslanGitelman <47743960+RuslanGitelman@users.noreply.github.com>
2021-04-12 14:09:44 +0300

    Initial commit

---
+++ cypress/integration/github_api.spec.js
@@ -0,0 +1,20 @@
+const testData = require('../support/test_data/test.data')
+
+describe('Github API tests', () => {
+
+    it('should get user by username', () => {
+        const url = testData.get_user.endpoint + testData.get_user.username,
+            token = testData.authorization.token,
+            user_id = testData.get_user.user_id,
+            node_id = testData.get_user.node_id
+        cy.request({
+            method: 'GET',
+            url: url,
+            headers: {'Authorization': 'Bearer ' + token}
+        }).then(response => {
+            expect(response.status).to.equal(200)
+            expect(response.body.id).to.equal(user_id)
+            expect(response.body.node_id).to.equal(node_id)
+        })
+    })
+})
---
+++ cypress/support/test_data/test.data.js
@@ -0,0 +1,12 @@
+module.exports = {
+    authorization: {
+        token: ""
+    },
+    get_user: {
+        endpoint: "/users/",
+        username: "mojombo",
+        user_id: 1,
+        node_id: "MDQ6VXNlcjE="
+    }
+
+}
